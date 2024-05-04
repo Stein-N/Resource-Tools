a690ab6acf6672f97724c9021344fa0b0225602b
Stein-N <nico-stein-98@outlook.de>
2024-05-04 19:31:45 +0200

    loader: register CrackerConfig

--- fabric/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
+++ fabric/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
@@ -1,6 +1,8 @@
 package net.xstopho.resource_cracker;
 
 import net.fabricmc.api.ModInitializer;
+import net.xstopho.resource_config_api.api.ConfigRegistry;
+import net.xstopho.resource_cracker.config.CrackerConfig;
 import net.xstopho.resource_cracker.registries.BlockRegistry;
 import net.xstopho.resource_cracker.registries.CreativeTabRegistry;
 import net.xstopho.resource_cracker.registries.ItemRegistry;
@@ -8,6 +10,8 @@ import net.xstopho.resource_cracker.registries.ItemRegistry;
 public class ResourceCracker implements ModInitializer {
     @Override
     public void onInitialize() {
+        ConfigRegistry.register(CrackerConstants.MOD_ID, CrackerConfig.BUILDER);
+
         BlockRegistry.init();
         ItemRegistry.init();
         CreativeTabRegistry.init();
--- neoforge/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
+++ neoforge/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
@@ -1,6 +1,8 @@
 package net.xstopho.resource_cracker;
 
 import net.neoforged.fml.common.Mod;
+import net.xstopho.resource_config_api.api.ConfigRegistry;
+import net.xstopho.resource_cracker.config.CrackerConfig;
 import net.xstopho.resource_cracker.registries.BlockRegistry;
 import net.xstopho.resource_cracker.registries.CreativeTabRegistry;
 import net.xstopho.resource_cracker.registries.ItemRegistry;
@@ -9,6 +11,8 @@ import net.xstopho.resource_cracker.registries.ItemRegistry;
 public class ResourceCracker {
 
     public ResourceCracker() {
+        ConfigRegistry.register(CrackerConstants.MOD_ID, CrackerConfig.BUILDER);
+
         BlockRegistry.init();
         ItemRegistry.init();
         CreativeTabRegistry.init();
