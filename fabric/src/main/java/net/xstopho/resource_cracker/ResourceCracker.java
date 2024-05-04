ded6db12926a67bc2ec14e0ddd3130435a942f2c
Stein-N <nico-stein-98@outlook.de>
2024-05-04 09:47:22 +0200

    loader: add registries to be initialized

--- fabric/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
+++ fabric/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
@@ -1,10 +1,15 @@
 package net.xstopho.resource_cracker;
 
 import net.fabricmc.api.ModInitializer;
+import net.xstopho.resource_cracker.registries.BlockRegistry;
+import net.xstopho.resource_cracker.registries.CreativeTabRegistry;
+import net.xstopho.resource_cracker.registries.ItemRegistry;
 
 public class ResourceCracker implements ModInitializer {
     @Override
     public void onInitialize() {
-
+        BlockRegistry.init();
+        ItemRegistry.init();
+        CreativeTabRegistry.init();
     }
 }
--- forge/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
+++ forge/src/main/java/net/xstopho/resource_cracker/ResourceCracker.java
@@ -1,11 +1,16 @@
 package net.xstopho.resource_cracker;
 
 import net.minecraftforge.fml.common.Mod;
+import net.xstopho.resource_cracker.registries.BlockRegistry;
+import net.xstopho.resource_cracker.registries.CreativeTabRegistry;
+import net.xstopho.resource_cracker.registries.ItemRegistry;
 
 @Mod(CrackerConstants.MOD_ID)
 public class ResourceCracker {
 
     public ResourceCracker() {
-
+        BlockRegistry.init();
+        ItemRegistry.init();
+        CreativeTabRegistry.init();
     }
 }
