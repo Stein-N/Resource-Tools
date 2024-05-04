c6cd3261b3361da6182630fd08d02cc1c4879219
Stein-N <nico-stein-98@outlook.de>
2024-05-04 10:20:46 +0200

    common: adding Lava and Water Spring Block

--- common/src/main/java/net/xstopho/resource_cracker/registries/BlockRegistry.java
+++ common/src/main/java/net/xstopho/resource_cracker/registries/BlockRegistry.java
@@ -8,6 +8,8 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.xstopho.resource_cracker.CrackerConstants;
 import net.xstopho.resource_cracker.block.GarlicCropBlock;
+import net.xstopho.resource_cracker.block.LavaSpringBlock;
+import net.xstopho.resource_cracker.block.WaterSpringBlock;
 import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
 
@@ -17,7 +19,14 @@ public class BlockRegistry {
 
     public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(Registries.BLOCK, CrackerConstants.MOD_ID);
 
-    public static final RegistryObject<Block> GARLIC_CROP = BLOCKS.register("garlic_crop", () -> new GarlicCropBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POTATOES)));
+    public static final RegistryObject<Block> GARLIC_CROP = BLOCKS.register("garlic_crop",
+            () -> new GarlicCropBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POTATOES)));
+
+    public static final RegistryObject<Block> WATER_SPRING_BLOCK = register("water_spring_block",
+            () -> new WaterSpringBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
+
+    public static final RegistryObject<Block> LAVA_SPRING_BLOCK = register("lava_spring_block",
+            () -> new LavaSpringBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK)));
 
     private static RegistryObject<Block> register(String id, Supplier<Block> block) {
         RegistryObject<Block> toReturn = BLOCKS.register(id, block);
