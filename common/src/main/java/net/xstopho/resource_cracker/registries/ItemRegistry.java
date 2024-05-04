1d2daee8d2884621fb5894a2bc059236352e99b2
Stein-N <nico-stein-98@outlook.de>
2024-05-04 19:32:40 +0200

    common: populate ItemRegistry with all Items

--- common/src/main/java/net/xstopho/resource_cracker/registries/ItemRegistry.java
+++ common/src/main/java/net/xstopho/resource_cracker/registries/ItemRegistry.java
@@ -3,7 +3,14 @@ package net.xstopho.resource_cracker.registries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemNameBlockItem;
+import net.minecraft.world.item.Tiers;
 import net.xstopho.resource_cracker.CrackerConstants;
+import net.xstopho.resource_cracker.config.CrackerConfig;
+import net.xstopho.resource_cracker.item.ChiselItem;
+import net.xstopho.resource_cracker.item.CrackHammerItem;
+import net.xstopho.resource_cracker.item.ScytheItem;
+import net.xstopho.resource_cracker.item.food.CrackerFoodProperties;
+import net.xstopho.resource_cracker.item.tier.CrackerToolTier;
 import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
 
@@ -13,7 +20,47 @@ public class ItemRegistry {
 
     public static final RegistryProvider<Item> ITEMS = RegistryProvider.get(Registries.ITEM, CrackerConstants.MOD_ID);
 
-    public static final RegistryObject<Item> GARLIC = register("garlic", () -> new ItemNameBlockItem(BlockRegistry.GARLIC_CROP.get(), new Item.Properties()));
+    public static final RegistryObject<Item> MATERIAL_DUST_COPPER = register("material_dust_copper");
+    public static final RegistryObject<Item> MATERIAL_DUST_IRON = register("material_dust_iron");
+    public static final RegistryObject<Item> MATERIAL_DUST_GOLD = register("material_dust_gold");
+    public static final RegistryObject<Item> MATERIAL_DUST_EMERALD = register("material_dust_emerald");
+    public static final RegistryObject<Item> MATERIAL_DUST_DIAMOND = register("material_dust_diamond");
+    public static final RegistryObject<Item> MATERIAL_DUST_NETHERITE_SCRAP = register("material_dust_netherite_scrap");
+    public static final RegistryObject<Item> MATERIAL_DUST_NETHERITE = register("material_dust_netherite");
+    public static final RegistryObject<Item> MATERIAL_DUST_CARBON = register("material_dust_carbon");
+    public static final RegistryObject<Item> MATERIAL_DUST_STEEL = register("material_dust_steel");
+    public static final RegistryObject<Item> MATERIAL_DUST_SULFUR = register("material_dust_sulfur");
+    public static final RegistryObject<Item> MATERIAL_DUST_SALTPETER = register("material_dust_saltpeter");
+
+    public static final RegistryObject<Item> STEEL_INGOT = register("steel_ingot");
+    public static final RegistryObject<Item> NUGGET_DIAMOND = register("nugget_diamond");
+    public static final RegistryObject<Item> NUGGET_EMERALD = register("nugget_emerald");
+    public static final RegistryObject<Item> NUGGET_COPPER = register("nugget_copper");
+
+    public static final RegistryObject<Item> CRACK_HAMMER_COPPER = register("crack_hammer_copper", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_COPPER.get()));
+    public static final RegistryObject<Item> CRACK_HAMMER_GOLD = register("crack_hammer_gold", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_GOLD.get()));
+    public static final RegistryObject<Item> CRACK_HAMMER_IRON = register("crack_hammer_iron", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_IRON.get()));
+    public static final RegistryObject<Item> CRACK_HAMMER_STEEL = register("crack_hammer_steel", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_STEEL.get()));
+    public static final RegistryObject<Item> CRACK_HAMMER_DIAMOND = register("crack_hammer_diamond", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_DIAMOND.get()));
+    public static final RegistryObject<Item> CRACK_HAMMER_NETHERITE = register("crack_hammer_netherite", () -> new CrackHammerItem(CrackerConfig.CRACK_HAMMER_NETHERITE.get()));
+
+    public static final RegistryObject<Item> CHISEL_COPPER = register("chisel_copper", () -> new ChiselItem(CrackerConfig.CHISEL_COPPER.get()));
+    public static final RegistryObject<Item> CHISEL_GOLD = register("chisel_gold", () -> new ChiselItem(CrackerConfig.CHISEL_GOLD.get()));
+    public static final RegistryObject<Item> CHISEL_IRON = register("chisel_iron", () -> new ChiselItem(CrackerConfig.CHISEL_IRON.get()));
+    public static final RegistryObject<Item> CHISEL_STEEL = register("chisel_steel", () -> new ChiselItem(CrackerConfig.CHISEL_STEEL.get()));
+    public static final RegistryObject<Item> CHISEL_DIAMOND = register("chisel_diamond", () -> new ChiselItem(CrackerConfig.CHISEL_DIAMOND.get()));
+    public static final RegistryObject<Item> CHISEL_NETHERITE = register("chisel_netherite", () -> new ChiselItem(CrackerConfig.CHISEL_NETHERITE.get()));
+
+    public static final RegistryObject<Item> SCYTHE_COPPER = register("scythe_copper", () -> new ScytheItem(Tiers.GOLD, 2, -1.8f));
+    public static final RegistryObject<Item> SCYTHE_GOLD = register("scythe_gold", () -> new ScytheItem(Tiers.GOLD, 3, -1.8f));
+    public static final RegistryObject<Item> SCYTHE_IRON = register("scythe_iron", () -> new ScytheItem(Tiers.IRON, 3, -1.8f));
+    public static final RegistryObject<Item> SCYTHE_STEEL = register("scythe_steel", () -> new ScytheItem(CrackerToolTier.STEEL, 3, -1.8f));
+    public static final RegistryObject<Item> SCYTHE_DIAMOND = register("scythe_diamond", () -> new ScytheItem(Tiers.DIAMOND, 4, -1.8f));
+    public static final RegistryObject<Item> SCYTHE_NETHERITE = register("scythe_netherite", () -> new ScytheItem(Tiers.NETHERITE, 5, -1.8f));
+
+    public static final RegistryObject<Item> GARLIC = register("garlic", () -> new ItemNameBlockItem(BlockRegistry.GARLIC_CROP.get(),
+            new Item.Properties().food(CrackerFoodProperties.GARLIC)));
+    public static final RegistryObject<Item> BEEF_JERKY = register("beef_jerky", () -> new Item(new Item.Properties().food(CrackerFoodProperties.BEEF_JERKY)));
 
     private static RegistryObject<Item> register(String id, Supplier<Item> item) {
         return ITEMS.register(id, item);
