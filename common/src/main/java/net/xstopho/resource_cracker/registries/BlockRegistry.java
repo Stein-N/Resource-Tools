3631763ca0a1ac0f974c9139df46df06bbc5ff25
Stein-N <nico-stein-98@outlook.de>
2024-05-04 09:47:53 +0200

    common: adding GarlicCropBlock

--- common/src/main/java/net/xstopho/resource_cracker/registries/BlockRegistry.java
+++ common/src/main/java/net/xstopho/resource_cracker/registries/BlockRegistry.java
@@ -4,7 +4,10 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.xstopho.resource_cracker.CrackerConstants;
+import net.xstopho.resource_cracker.block.GarlicCropBlock;
 import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
 
@@ -14,7 +17,7 @@ public class BlockRegistry {
 
     public static final RegistryProvider<Block> BLOCKS = RegistryProvider.get(Registries.BLOCK, CrackerConstants.MOD_ID);
 
-
+    public static final RegistryObject<Block> GARLIC_CROP = BLOCKS.register("garlic_crop", () -> new GarlicCropBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POTATOES)));
 
     private static RegistryObject<Block> register(String id, Supplier<Block> block) {
         RegistryObject<Block> toReturn = BLOCKS.register(id, block);
--- common/src/main/java/net/xstopho/resource_cracker/registries/ItemRegistry.java
+++ common/src/main/java/net/xstopho/resource_cracker/registries/ItemRegistry.java
@@ -2,6 +2,7 @@ package net.xstopho.resource_cracker.registries;
 
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.xstopho.resource_cracker.CrackerConstants;
 import net.xstopho.resourcelibrary.registration.RegistryObject;
 import net.xstopho.resourcelibrary.registration.RegistryProvider;
@@ -12,7 +13,7 @@ public class ItemRegistry {
 
     public static final RegistryProvider<Item> ITEMS = RegistryProvider.get(Registries.ITEM, CrackerConstants.MOD_ID);
 
-
+    public static final RegistryObject<Item> GARLIC = register("garlic", () -> new ItemNameBlockItem(BlockRegistry.GARLIC_CROP.get(), new Item.Properties()));
 
     private static RegistryObject<Item> register(String id, Supplier<Item> item) {
         return ITEMS.register(id, item);
