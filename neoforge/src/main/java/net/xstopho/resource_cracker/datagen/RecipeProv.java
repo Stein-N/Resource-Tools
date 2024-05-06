17d971ed47d3ebdb20b25f3a840293e172b2d5fe
Stein-N <nico-stein-98@outlook.de>
2024-05-06 21:08:35 +0200

    project: adding Mod Recipes to Datagen

--- neoforge/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
+++ neoforge/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
@@ -4,10 +4,13 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
+import net.neoforged.neoforge.common.conditions.ICondition;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.xstopho.resource_cracker.datagen.compat.mods.AE2Recipes;
 
 import java.util.concurrent.CompletableFuture;
 
-public class RecipeProv extends RecipeProvider {
+public class RecipeProv extends RecipeProvider implements IConditionBuilder {
 
 
     public RecipeProv(PackOutput output, CompletableFuture<HolderLookup.Provider> provider) {
@@ -16,11 +19,20 @@ public class RecipeProv extends RecipeProvider {
 
     @Override
     protected void buildRecipes(RecipeOutput output) {
+
+        AE2Recipes.generate(output.withConditions(getConditions(modLoaded("ae2"))));
+
         CrackerRecipes.generateToolRecipes(output);
         CrackerRecipes.generateMaterialDustRecipes(output);
         CrackerRecipes.generateSpringBlockRecipes(output);
         CrackerRecipes.generateProcessingRecipes(output);
         CrackerRecipes.generateCompactingRecipes(output);
         CrackerRecipes.generateOtherRecipes(output);
+    }
+
+    static ICondition[] getConditions(ICondition... conditions) {
+        ICondition[] con = new ICondition[conditions.length];
+        System.arraycopy(conditions, 0, con, 0, con.length);
+        return con;
     }
 }
