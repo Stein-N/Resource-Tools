08823e19928b0c57280adb3c69ec7419f55058fe
Stein-N <nico-stein-98@outlook.de>
2024-06-15 22:22:27 +0200

    fabric: adding Tech Reborn Recipes to Datagen

--- fabric/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
+++ fabric/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
@@ -2,14 +2,10 @@ package net.xstopho.resource_cracker.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
-import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.Items;
-import net.xstopho.resource_cracker.item.tags.CrackerItemTags;
-import net.xstopho.resource_cracker.registries.ItemRegistry;
+import net.xstopho.resource_cracker.datagen.compat.mods.TechRebornRecipes;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -22,6 +18,8 @@ public class RecipeProv extends FabricRecipeProvider {
 
     @Override
     public void buildRecipes(RecipeOutput output) {
+
+        TechRebornRecipes.generate(withConditions(output, ResourceConditions.allModsLoaded("techreborn")));
 
         CrackerRecipes.generateToolRecipes(output);
         CrackerRecipes.generateMaterialDustRecipes(output);
