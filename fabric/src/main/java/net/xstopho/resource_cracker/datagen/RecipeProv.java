28fbaa82f9238b1bc74f9c6b07473d35b56cb367
Stein-N <nico-stein-98@outlook.de>
2024-12-05 12:56:40 +0100

    project: remove compat recipe datagen, will be created in another branch and published as datapacks

--- fabric/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
+++ fabric/src/main/java/net/xstopho/resource_cracker/datagen/RecipeProv.java
@@ -2,11 +2,9 @@ package net.xstopho.resource_cracker.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.fabricmc.fabric.api.resource.conditions.v1.ResourceConditions;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
-import net.xstopho.resource_cracker.datagen.compat.mods.TechRebornRecipes;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -23,14 +21,12 @@ public class RecipeProv extends FabricRecipeProvider {
             @Override
             public void buildRecipes() {
                 super.buildRecipes();
-
-                new TechRebornRecipes(provider, withConditions(recipeOutput, ResourceConditions.allModsLoaded("techreborn")));
             }
         };
     }
 
     @Override
     public String getName() {
-        return "Recipe Provider";
+        return "Resource Cracker Recipes";
     }
 }
