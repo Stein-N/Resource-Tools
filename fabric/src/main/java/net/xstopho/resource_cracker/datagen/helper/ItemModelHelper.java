ed571fded24362a40ca0fb4560b5b30db3578d98
Stein-N <nico-stein-98@outlook.de>
2024-12-05 13:44:38 +0100

    fabric: create new helper method to generate scythe model json with different DisplayContext Properties

--- fabric/src/main/java/net/xstopho/resource_cracker/datagen/helper/ItemModelHelper.java
+++ fabric/src/main/java/net/xstopho/resource_cracker/datagen/helper/ItemModelHelper.java
@@ -1,26 +1,57 @@
 package net.xstopho.resource_cracker.datagen.helper;
 
+import net.minecraft.client.data.models.ItemModelGenerators;
+import net.minecraft.client.data.models.model.*;
+import net.minecraft.client.renderer.item.SelectItemModel;
+import net.minecraft.client.renderer.item.properties.select.DisplayContext;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.data.models.ItemModelGenerators;
-import net.minecraft.data.models.model.ModelTemplate;
-import net.minecraft.data.models.model.TextureMapping;
-import net.minecraft.data.models.model.TextureSlot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemDisplayContext;
 import net.xstopho.resource_cracker.CrackerConstants;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ItemModelHelper {
 
-    private static final ResourceLocation PARENT = ResourceLocation.fromNamespaceAndPath(CrackerConstants.MOD_ID, "item/in_hand/handheld_large");
+    private static final ResourceLocation PARENT_FLAT = ResourceLocation.withDefaultNamespace("item/handheld");
+    private static final ResourceLocation PARENT_FLAT_LARGE = ResourceLocation.fromNamespaceAndPath(CrackerConstants.MOD_ID, "item/in_hand/handheld_large");
 
-    public static void createInHandItemModel(ItemModelGenerators generator, Item item) {
-        new ModelTemplate(Optional.of(PARENT), Optional.empty(), TextureSlot.LAYER0)
-                .create(modifyItemKey(item), TextureMapping.layer0(modifyItemKey(item)), generator.output);
+    public static void generateScytheModels(ItemModelGenerators generator, Item item) {
+        createBaseModels(generator, item);
+        generator.itemModelOutput.accept(item,
+                ItemModelUtils.select(new DisplayContext(),
+                        ItemModelUtils.plainModel(normalModelKey(item)),
+                        new SelectItemModel.SwitchCase<>(
+                                List.of(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND), ItemModelUtils.plainModel(inHandModelKey(item))
+                        ),
+                        new SelectItemModel.SwitchCase<>(
+                                List.of(ItemDisplayContext.FIRST_PERSON_LEFT_HAND), ItemModelUtils.plainModel(inHandModelKey(item))
+                        ),
+                        new SelectItemModel.SwitchCase<>(
+                                List.of(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND), ItemModelUtils.plainModel(inHandModelKey(item))
+                        ),
+                        new SelectItemModel.SwitchCase<>(
+                                List.of(ItemDisplayContext.THIRD_PERSON_LEFT_HAND), ItemModelUtils.plainModel(inHandModelKey(item))
+                        )
+                ));
     }
 
-    private static ResourceLocation modifyItemKey(Item item) {
+    private static void createBaseModels(ItemModelGenerators generator, Item item) {
+        new ModelTemplate(Optional.of(PARENT_FLAT), Optional.empty(), TextureSlot.LAYER0)
+                .create(normalModelKey(item), TextureMapping.layer0(normalModelKey(item)), generator.modelOutput);
+
+        new ModelTemplate(Optional.of(PARENT_FLAT_LARGE), Optional.empty(), TextureSlot.LAYER0)
+                .create(inHandModelKey(item), TextureMapping.layer0(inHandModelKey(item)), generator.modelOutput);
+    }
+
+    private static ResourceLocation normalModelKey(Item item) {
+        ResourceLocation key = getItemKey(item);
+        return ResourceLocation.fromNamespaceAndPath(key.getNamespace(), "item/" + key.getPath());
+    }
+
+    private static ResourceLocation inHandModelKey(Item item) {
         ResourceLocation key = getItemKey(item);
         return ResourceLocation.fromNamespaceAndPath(key.getNamespace(), "item/in_hand/" + key.getPath());
     }
